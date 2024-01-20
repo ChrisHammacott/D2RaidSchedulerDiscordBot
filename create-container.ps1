38f695db567075f20e8c2043c4bacd5e92ad7c92
Christian Hammacott <cahammacott@gmail.com>
2024-01-20 16:40:05 +0000

    Getting ready for 1.0.0 deployment

---
+++ create-container.ps1
@@ -0,0 +1,15 @@
+$VERSION="1.0.0"
+
+mvn clean install -DskipTests
+
+docker buildx create --use --name buildx_instance
+
+#build x86 version
+docker buildx build -t raidschedulerbot:$VERSION --build-arg BOT_VERSION=$VERSION --load .
+#build arm version
+docker buildx build --platform linux/arm64 -t raidschedulerbot-arm:$VERSION --build-arg BOT_VERSION=$VERSION --load .
+
+docker buildx stop buildx_instance
+
+#save image to deploy to server
+docker save --output raidschedulerbot-arm-$VERSION.tar raidschedulerbot-arm:$VERSION
